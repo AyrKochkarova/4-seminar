324481b41b323cdd2f310dc04bd2d4a3be3f1c38
Ayra <777aira777@gmail.com>
2022-10-15 22:53:34 +0400

    Расширена функциональность решения 25 задачи

--- Task25/Program.cs
+++ Task25/Program.cs
@@ -2,26 +2,72 @@
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
 
+// Console.WriteLine("Введите число:");
+// int a = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите в какую степень нужно возвести:");
+// int b = Convert.ToInt32(Console.ReadLine());
+
+// while (b <= 0)
+// {
+//     Console.WriteLine("Число должно быть натуральным. Повторите ввод:");
+//     b = Convert.ToInt32(Console.ReadLine());
+// }
+
+// int degreeOfNum = DegreeOfNum(a, b);
+// Console.WriteLine($"{a} в степени {b} -> {degreeOfNum}");
+
+// int DegreeOfNum(int num, int degree)
+// {
+//     int result = 1;
+//     for (int i = 1; i <= degree; i++)
+//     {
+//     result = result * num;
+//     }
+//     return result;
+// }
+
 Console.WriteLine("Введите число:");
 int a = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Введите в какую степень нужно возвести:");
 int b = Convert.ToInt32(Console.ReadLine());
 
-while (b <= 0)
+if (b == 0 || a == 0 && b == 0)
+    Console.WriteLine($"{a} в степени 0 -> 1");
+
+else if (a == 0)
+    Console.WriteLine($"0 в степени {b} -> 0");
+
+else if (b < 0)
 {
-    Console.WriteLine("Число должно быть натуральным. Повторите ввод:");
-    b = Convert.ToInt32(Console.ReadLine());
+    double degreeOfNum = Math.Round(NegativeDegreeOfNum(a, b), 10, MidpointRounding.ToZero);
+    Console.WriteLine($"{a} в степени {b} -> {degreeOfNum}");
+}
+else
+{
+    int degreeOfNum = DegreeOfNum(a, b);
+    Console.WriteLine($"{a} в степени {b} -> {degreeOfNum}");
 }
 
-int degreeOfNum = DegreeOfNum(a, b);
-Console.WriteLine($"{a} в степени {b} -> {degreeOfNum}");
 
 int DegreeOfNum(int num, int degree)
 {
     int result = 1;
     for (int i = 1; i <= degree; i++)
     {
-    result = result * num;
+        result = result * num;
     }
+    return result;
+}
+
+
+double NegativeDegreeOfNum(int num, int degree)
+{
+    int myInt = System.Math.Abs(degree);
+    double result = 1;
+    for (int i = 1; i <= myInt; i++)
+    {
+        result = result * num;
+    }
+    result = 1 / result;
     return result;
 }
